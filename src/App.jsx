5d232c62b1f615fcfd189b96083644a0d3a4401b
theDAOS <sanjusanthosh46@gmail.com>
2025-04-28 20:03:21 +0530

    Card

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Button } from '@mui/material'
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import './App.css'
+import BasicCard from './components/Card';
 
 const theme = createTheme({
   colorSchemes: {
@@ -15,6 +16,7 @@ function App() {
       <CssBaseline />
       <main>This app is using the dark mode</main>
       <Button variant="contained">Hello world</Button>
+      <BasicCard />
     </ThemeProvider>
   )
 }
