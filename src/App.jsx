63fcc1ed9a5c2043a4a7258d3c9aca2317ab85c5
theDAOS <sanjusanthosh46@gmail.com>
2025-04-28 20:04:43 +0530

    ResponsiveAppBar

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import './App.css'
 import BasicCard from './components/Card';
+import ResponsiveAppBar from './components/NavBar';
 
 const theme = createTheme({
   colorSchemes: {
@@ -14,6 +15,7 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
+      <ResponsiveAppBar />
       <main>This app is using the dark mode</main>
       <Button variant="contained">Hello world</Button>
       <BasicCard />
