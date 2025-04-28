000a61ea3d8661498beddd068f582cc5ad28e30a
theDAOS <sanjusanthosh46@gmail.com>
2025-04-28 19:57:02 +0530

    system preference theme mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { Button } from '@mui/material'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import './App.css'
 
-function App() {
+const theme = createTheme({
+  colorSchemes: {
+    dark: true,
+  },
+});
 
+function App() {
   return (
-    <>
-      <h1>Hello Vite</h1>
-    </>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <main>This app is using the dark mode</main>
+      <Button variant="contained">Hello world</Button>
+    </ThemeProvider>
   )
 }
 
